98fd3d51732db398e67217f079da821a83bc5007
DinHoo <51788868+DinHoo@users.noreply.github.com>
2019-07-23 11:39:26 -0300

    inimigo random

---
+++ Lista De Exerc/Assets/Scripts/Game.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] listaPrefabInimigos = new GameObject[4];
+
+    // lista com posições para spawnar os inimigos
+    public List<Transform> spawnPointInimigos = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Exercicio1")
+            StartExe1();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartExe1()
+    {
+        if (GameObject.FindGameObjectWithTag("InimigoSpawn"))
+        {
+            print("achou");
+            foreach (Transform filho in GameObject.FindGameObjectWithTag("InimigoSpawn").transform)
+            {
+                spawnPointInimigos.Add(filho);
+            }
+                       
+        }
+
+        spawnInimigo(listaPrefabInimigos[Random.Range(0, listaPrefabInimigos.Length)]);
+    }
+
+    //função para spawnar inimigos em determinada posição
+    private void spawnInimigo(GameObject prefab)
+    {
+        if (!spawnPointInimigos.Any()) return;
+
+        Vector3 position = spawnPointInimigos[Random.Range(0, spawnPointInimigos.Count)].position;
+
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
---
+++ Lista De Exerc/Assets/Scripts/Inimigo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public abstract class Inimigo : MonoBehaviour
+{
+    [SerializeField]
+    protected float speed;
+    [SerializeField]
+    protected Vector2 direction;
+    [SerializeField]
+    protected Transform target;
+    [SerializeField]
+    protected float _tipo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (SceneManager.GetActiveScene().name == "Exercicio1")
+
+    }
+
+    public void Init()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        direction = target.position - transform.position;
+        direction = direction.normalized;
+    }
+
+    public virtual void Move()
+    {
+        Vector2 velocity = speed * direction * Time.deltaTime;
+        transform.Translate(velocity);
+
+    }
+}
